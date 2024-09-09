aa36b9f7fb627e169f3c7ec4d0aca9c986bfae39
Greg Linklater <glinklater@linklater.no>
2024-09-09 10:50:47 +0200

    Add pg_uuidv7

--- install_pg_extensions.sh
+++ install_pg_extensions.sh
@@ -34,6 +34,21 @@ for EXTENSION in ${EXTENSIONS}; do
         continue
     fi
 
+    if [ "$EXTENSION" == "pg_uuidv7" ]; then
+        TEMP_DIR="$(mktemp -d)"
+        cd $TEMP_DIR
+        
+        curl -LO "https://github.com/fboulnois/pg_uuidv7/releases/download/v1.5.0/{pg_uuidv7.tar.gz,SHA256SUMS}" 
+        tar xf pg_uuidv7.tar.gz
+        sha256sum -c SHA256SUMS
+        PG_MAJOR=$(pg_config --version | sed 's/^.* \([0-9]\{1,\}\).*$/\1/')
+        cp "$PG_MAJOR/pg_uuidv7.so" "$(pg_config --pkglibdir)"
+        cp pg_uuidv7--1.5.sql pg_uuidv7.control "$(pg_config --sharedir)/extension"
+        rm -r $TEMP_DIR
+
+        continue
+    fi
+
     # is it an extension found in apt?
     if apt-cache show "postgresql-${PG_MAJOR}-${EXTENSION}" &> /dev/null; then
         # install the extension
